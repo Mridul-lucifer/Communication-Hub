63b02b6dbcaf29c789d7bcadeb400dfa97346311
Mridul-lucifer <124557704+Mridul-lucifer@users.noreply.github.com>
2024-09-18 21:12:27 +0530

    Update Displaychat.js

--- user/src/Components/Displaychat.js
+++ user/src/Components/Displaychat.js
@@ -46,17 +46,17 @@ export default function DisplayChat() {
 
   return (
     <div>
-      <h2 className='DisplayChat-header'>{groupName}</h2>
-      {error && <div className='error-message'>{error}</div>}
+      <h2 className='DisplayChat-header'>{GroupName}</h2>
       <div className='DisplayChat-container'>
-        {array.map((chat, index) => (
+        {array.map(chat => (
           <div 
-            key={index} // Using index as key for simplicity; consider a unique id if available
+            key={chat.id}  // Ensure to use a unique key for each element
             className='DisplayChat-box'
           >
             {chat.Name === myName ? (
               <div className='DisplayChat-myMessage'>
                 <span className='DisplayChat-msg-yourmessage'>{chat.msg}</span>
+                {/* <span className='DisplayChat-id-you'>(You)</span> */}
               </div>
             ) : (
               <div className='DisplayChat-otherMessage'>
