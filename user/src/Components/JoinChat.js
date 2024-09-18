4add64180c701bdf16ccb358db06830b78f00a67
Mridul-lucifer <124557704+Mridul-lucifer@users.noreply.github.com>
2024-09-18 15:51:39 +0530

    Update JoinChat.js

--- user/src/Components/JoinChat.js
+++ user/src/Components/JoinChat.js
@@ -17,7 +17,7 @@ export default function JoinChat() {
         Passcode:Passcode,
         token : token
       })
-      alert(response.data.msg+" " +response.data.msg);
+      alert(response.data.msg+" " +response.data.GroupNo);
       if(response.data.GroupNo){
         localStorage.setItem("GroupNo" , response.data.GroupNo)
         navigate('/ChatingGroup')
